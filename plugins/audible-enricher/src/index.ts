0bf298ac1badc3c162d1d38c79475c7b52f9ed0a
Robin Joseph <robinmjoseph08@gmail.com>
2026-04-05 21:56:43 -0500

    [Feat] Add Audible enricher plugin entry point

---
+++ plugins/audible-enricher/src/index.ts
@@ -0,0 +1,22 @@
+import { searchForBooks } from "./lookup";
+import type {
+  SearchContext,
+  SearchResponse,
+  ShishoPlugin,
+} from "@shisho/plugin-sdk";
+
+const plugin: ShishoPlugin = {
+  metadataEnricher: {
+    search(context: SearchContext): SearchResponse {
+      shisho.log.info("Audible enricher: searching");
+
+      const results = searchForBooks(context);
+      shisho.log.info(`Found ${results.length} candidate(s)`);
+
+      return { results };
+    },
+  },
+};
+
+// Export for esbuild IIFE bundling - this becomes the return value
+export default plugin;
